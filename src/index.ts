86990a03f84481b4942f6e27d5eaca7745ca3e95
Hornwitser <github@hornwitser.no>
2021-08-25 04:16:50 +0200

    Configure for publishing
    Add barrel export of all types for consumers of the project and set up
    package.json and TypeScript config to only build and package the src
    directory.

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from "./stream";
+export * from "./data";
+export * from "./input";
+export * from "./synchronizer_action";
+export * from "./network_message";
+export { default as NetworkFrame } from "./NetworkFrame";
+export { default as UdpClient } from "./UdpClient";
+export { default as FactorioClient } from "./FactorioClient";
